f75b89c18d9f4b83cfadebdb7db71ae908b1fef9
AnaniKassa <kass0112@algonquinlive.com>
2026-03-16 14:19:44 -0400

    tflint & terratest succeeded

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -8,41 +8,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+// Use your assigned subscription under the Cloud Dev/Ops program tenant
+var subscriptionID string = "627b10ce-37e2-41c0-bfe6-bd444b154bd9"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
+		// Path to your Terraform code
 		TerraformDir: "../",
-		// Override the default terraform variables
+		// Override default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "kass0112",
 		},
 	}
 
+	// Clean up resources with `terraform destroy` at the end
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	// Run `terraform init` and `terraform apply`
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
+	// Get Terraform outputs
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	nicName := terraform.Output(t, terraformOptions, "nic_name")
 
-	// Confirm VM exists
+	// --- Test 1: Confirm VM exists ---
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
 
-	// Confirm NIC exists and is connected to VM
-	actualNicNames := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
-	assert.Equal(t, nicName, actualNicNames[0])
+	// --- Test 2: Confirm NIC exists and is attached to VM ---
+	assert.True(t, azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID))
 
-	// Confirm the VM is running the correct Ubuntu version
-	vmImage := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
-	expectedOSPublisher := "Canonical"
-	expectedOSVersion := "22_04-lts-gen2"
-	assert.Equal(t, expectedOSPublisher, vmImage.Publisher)
-	assert.Equal(t, expectedOSVersion, vmImage.SKU)
+	vm := azure.GetVirtualMachine(t, vmName, resourceGroupName, subscriptionID)
+
+	// Dereference the pointer to get the slice
+	nics := *vm.NetworkProfile.NetworkInterfaces
+	if len(nics) == 0 {
+		t.Fatal("VM has no network interfaces attached")
+	}
+
+	// Confirm one of the attached NICs matches the expected NIC
+	foundNIC := false
+	for _, nic := range nics {
+		if *nic.ID != "" && (*nic.ID)[len(*nic.ID)-len(nicName):] == nicName {
+			foundNIC = true
+			break
+		}
+	}
+	assert.True(t, foundNIC, "Expected NIC is not attached to the VM")
+
+	// --- Test 3: Confirm VM is running correct Ubuntu version ---
+	// Use ImageReference.Sku from VM storage profile
+	expectedSKU := "22_04-lts-gen2"
+	actualSKU := *vm.StorageProfile.ImageReference.Sku
+	assert.Equal(t, expectedSKU, actualSKU, "VM is not running the expected Ubuntu version")
 }
